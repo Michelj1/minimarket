e4e20bbef60555bb621c6cd2635704f485257e0c
Emanuel <102432747+Minorbutmajor10@users.noreply.github.com>
2022-05-23 15:41:31 -0500

    crud cliente

--- src/main/java/gui/FrmCrudCliente.java
+++ src/main/java/gui/FrmCrudCliente.java
@@ -1,27 +1,54 @@
 package gui;
 
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.List;
+
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import entidad.Cliente;
+import model.ClienteModel;
+import util.Validaciones;
+
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
-public class FrmCrudCliente extends JInternalFrame {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
+public class FrmCrudCliente extends JInternalFrame implements ActionListener, MouseListener, KeyListener {
+	private JTextField txtNOMBRE;
+	private JTextField txtAPELLIDO;
+	private JTextField txtDNI;
+	private JTextField txtTELEF;
+	private JTextField txtDIREC;
+	private JScrollPane scrollPane;
+	private JComboBox<String> cboPAIS;
+	private JComboBox<String> cboCOMPROB;
 	private JTable table;
+	int idSeleccionado =-1;
+	int hoveredRow = -1, hoveredColumn = -1;
+	private JButton btnREGISTRAR_1;
+	private JButton btnACTUALIZAR_1;
+	private JButton btnELIMINAR_1;
 
 	/**
 	 * Launch the application.
@@ -60,50 +87,55 @@ public class FrmCrudCliente extends JInternalFrame {
 		lblNewLabel_1.setBounds(96, 88, 81, 13);
 		getContentPane().add(lblNewLabel_1);
 		
-		textField = new JTextField();
-		textField.setBounds(187, 86, 255, 19);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		txtNOMBRE = new JTextField();
+		txtNOMBRE.addKeyListener(this);
+		txtNOMBRE.setBounds(187, 86, 255, 19);
+		getContentPane().add(txtNOMBRE);
+		txtNOMBRE.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Apellido :");
 		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblNewLabel_2.setBounds(96, 134, 81, 19);
 		getContentPane().add(lblNewLabel_2);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(187, 135, 255, 20);
-		getContentPane().add(textField_1);
+		txtAPELLIDO = new JTextField();
+		txtAPELLIDO.addKeyListener(this);
+		txtAPELLIDO.setColumns(10);
+		txtAPELLIDO.setBounds(187, 135, 255, 20);
+		getContentPane().add(txtAPELLIDO);
 		
 		JLabel lblNewLabel_3 = new JLabel("DNI :");
 		lblNewLabel_3.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblNewLabel_3.setBounds(96, 180, 81, 19);
 		getContentPane().add(lblNewLabel_3);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(187, 181, 255, 19);
-		getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		txtDNI = new JTextField();
+		txtDNI.addKeyListener(this);
+		txtDNI.setBounds(187, 181, 255, 19);
+		getContentPane().add(txtDNI);
+		txtDNI.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Tel\u00E9fono :");
 		lblNewLabel_4.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblNewLabel_4.setBounds(96, 226, 81, 19);
 		getContentPane().add(lblNewLabel_4);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(187, 227, 255, 19);
-		getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		txtTELEF = new JTextField();
+		txtTELEF.addKeyListener(this);
+		txtTELEF.setBounds(187, 227, 255, 19);
+		getContentPane().add(txtTELEF);
+		txtTELEF.setColumns(10);
 		
 		JLabel lblNewLabel_5 = new JLabel("Direcci\u00F3n :");
 		lblNewLabel_5.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblNewLabel_5.setBounds(529, 88, 120, 13);
 		getContentPane().add(lblNewLabel_5);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(644, 86, 244, 19);
-		getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		txtDIREC = new JTextField();
+		txtDIREC.addKeyListener(this);
+		txtDIREC.setBounds(644, 86, 244, 19);
+		getContentPane().add(txtDIREC);
+		txtDIREC.setColumns(10);
 		
 		JLabel lblNewLabel_6 = new JLabel("Pa\u00EDs :");
 		lblNewLabel_6.setFont(new Font("Arial", Font.PLAIN, 14));
@@ -115,19 +147,34 @@ public class FrmCrudCliente extends JInternalFrame {
 		lblNewLabel_7.setBounds(529, 186, 120, 13);
 		getContentPane().add(lblNewLabel_7);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(644, 180, 244, 21);
-		getContentPane().add(comboBox);
+		cboCOMPROB= new JComboBox();
+		cboCOMPROB.addItem("Sleccinar");
+		cboCOMPROB.addItem("Boleta");
+		cboCOMPROB.addItem("Factura");
+		cboCOMPROB.setBounds(644, 180, 244, 21);
+		getContentPane().add(cboCOMPROB);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(644, 134, 244, 21);
-		getContentPane().add(comboBox_1);
+		cboPAIS = new JComboBox<String>();
+		cboPAIS.addItem("[Seleccione]");
+		cboPAIS.addItem("Perú");
+		cboPAIS.addItem("Colombia");
+		cboPAIS.addItem("Argentina");
+		cboPAIS.addItem("Bolivia");
+		cboPAIS.addItem("Chile");
+		cboPAIS.addItem("Uruguay");
+		cboPAIS.addItem("Venezuela");
+		cboPAIS.addItem("Brasil");
+		cboPAIS.addItem("Paraguay");
+		cboPAIS.addItem("España");
+		cboPAIS.setBounds(644, 134, 244, 21);
+		getContentPane().add(cboPAIS);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 411, 992, 267);
 		getContentPane().add(scrollPane);
 		
 		table = new JTable();
+		table.addMouseListener(this);
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
 			},
@@ -137,23 +184,324 @@ public class FrmCrudCliente extends JInternalFrame {
 		));
 		scrollPane.setViewportView(table);
 		
-		JButton btnNewButton = new JButton("REGISTRAR");
-		btnNewButton.setIcon(new ImageIcon(FrmCrudCliente.class.getResource("/iconos/Add.gif")));
-		btnNewButton.setFont(new Font("Arial", Font.BOLD, 22));
-		btnNewButton.setBounds(96, 312, 205, 46);
-		getContentPane().add(btnNewButton);
+		btnREGISTRAR_1 = new JButton("REGISTRAR");
+		btnREGISTRAR_1.addActionListener(this);
+		btnREGISTRAR_1.setIcon(new ImageIcon(FrmCrudCliente.class.getResource("/iconos/Add.gif")));
+		btnREGISTRAR_1.setFont(new Font("Arial", Font.BOLD, 22));
+		btnREGISTRAR_1.setBounds(96, 312, 205, 46);
+		getContentPane().add(btnREGISTRAR_1);
 		
-		JButton btnNewButton_1 = new JButton("ACTUALIZAR");
-		btnNewButton_1.setIcon(new ImageIcon(FrmCrudCliente.class.getResource("/iconos/Gear.gif")));
-		btnNewButton_1.setFont(new Font("Arial", Font.BOLD, 22));
-		btnNewButton_1.setBounds(407, 312, 205, 46);
-		getContentPane().add(btnNewButton_1);
+		btnACTUALIZAR_1 = new JButton("ACTUALIZAR");
+		btnACTUALIZAR_1.addActionListener(this);
+		btnACTUALIZAR_1.setIcon(new ImageIcon(FrmCrudCliente.class.getResource("/iconos/Gear.gif")));
+		btnACTUALIZAR_1.setFont(new Font("Arial", Font.BOLD, 22));
+		btnACTUALIZAR_1.setBounds(407, 312, 205, 46);
+		getContentPane().add(btnACTUALIZAR_1);
 		
-		JButton btnNewButton_2 = new JButton("ELIMINAR");
-		btnNewButton_2.setIcon(new ImageIcon(FrmCrudCliente.class.getResource("/iconos/Delete.gif")));
-		btnNewButton_2.setFont(new Font("Arial", Font.BOLD, 22));
-		btnNewButton_2.setBounds(704, 312, 205, 46);
-		getContentPane().add(btnNewButton_2);
+		btnELIMINAR_1 = new JButton("ELIMINAR");
+		btnELIMINAR_1.addActionListener(this);
+		btnELIMINAR_1.setIcon(new ImageIcon(FrmCrudCliente.class.getResource("/iconos/Delete.gif")));
+		btnELIMINAR_1.setFont(new Font("Arial", Font.BOLD, 22));
+		btnELIMINAR_1.setBounds(704, 312, 205, 46);
+		getContentPane().add(btnELIMINAR_1);
+		
+		//selecciona una sola fila
+		table.setRowSelectionAllowed(true);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+        //desabilita mover las columnas
+		table.getTableHeader().setReorderingAllowed(false);
+		
+		//color de la fila seleccionada
+		table.setSelectionBackground(Color.BLUE);
+		
+		//No se pueda editar
+	    table.setDefaultEditor(Object.class, null);
+	    
+		//Efecto Rollover
+	    table.addMouseMotionListener(new MouseMotionListener() {
+	        @Override
+	        public void mouseMoved(MouseEvent e) {
+	            Point p = e.getPoint();
+	            hoveredRow = table.rowAtPoint(p);
+	            hoveredColumn = table.columnAtPoint(p);
+	            table.setRowSelectionInterval(hoveredRow, hoveredRow);
+	            table.repaint();    
+	        }
+	        @Override
+	        public void mouseDragged(MouseEvent e) {
+	            hoveredRow = hoveredColumn = -1;
+	            table.repaint();
+	        }
+	    });
+	    //mostrando la tabla
+	    lista();
 
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnELIMINAR_1) {
+			do_btnELIMINAR_actionPerformed(e);
+		}
+		if (e.getSource() == btnACTUALIZAR_1) {
+			do_btnACTUALIZAR_actionPerformed(e);
+		}
+		if (e.getSource() == btnREGISTRAR_1) {
+			do_btnREGISTRAR_actionPerformed(e);
+		}
+	}
+	protected void do_btnREGISTRAR_actionPerformed(ActionEvent e) {
+		inserta();
+	}
+	protected void do_btnACTUALIZAR_actionPerformed(ActionEvent e) {
+		actualiza();
+	}
+	protected void do_btnELIMINAR_actionPerformed(ActionEvent e) {
+		elimina();
+	}
+	public void mouseClicked(MouseEvent e) {
+		if (e.getSource() == table) {
+			do_table_mouseClicked(e);
+		}
+	}
+	public void mouseEntered(MouseEvent e) {
+	}
+	public void mouseExited(MouseEvent e) {
+	}
+	public void mousePressed(MouseEvent e) {
+	}
+	public void mouseReleased(MouseEvent e) {
+	}
+	protected void do_table_mouseClicked(MouseEvent e) {
+		busca();
+	}
+	void limpiarCajasTexto() {
+		txtNOMBRE.setText("");
+		txtAPELLIDO.setText("");
+		txtDNI.setText("");
+		txtTELEF.setText("");
+		txtDIREC.setText("");
+		txtNOMBRE.requestFocus();
+	}
+	public void mensaje(String ms) {
+		JOptionPane.showMessageDialog(this, ms);
+	}
+	private void lista() {
+		ClienteModel model = new ClienteModel();
+		List<Cliente> lista = model.listaCliente();
+
+		DefaultTableModel dtm = (DefaultTableModel) table.getModel();
+		dtm.setRowCount(0);
+
+		Object[] fila = null;
+		for (Cliente x : lista) {
+			fila = new Object[] { x.getIdCliente(), 
+					x.getNombre(), 
+					x.getApellido(),
+					x.getDni(),
+					x.getDireccion(),
+					x.getTelefono(),
+					x.getPais(),
+					x.getComprobante()};
+			dtm.addRow(fila);
+		}
+	}
+	private void inserta() {
+		String nom = txtNOMBRE.getText();
+		String ape = txtAPELLIDO.getText();
+		String dni = txtDNI.getText();
+		String dir = txtDIREC.getText();
+		String tel = txtTELEF.getText();
+		String pai = cboPAIS.getSelectedItem().toString();
+		String com = cboCOMPROB.getSelectedItem().toString();
+
+		if (!nom.matches(Validaciones.TEXTO)) {
+			mensaje("El nombre es de 2 a 20 caracteres");
+		} else if (!ape.matches(Validaciones.TEXTO)) {
+			mensaje("El apellido es de 2 a 20 caracteres");
+		} else if (!dni.matches(Validaciones.DNI)) {
+			mensaje("El DNI es de 8 digitos");
+		}else if (!tel.matches(Validaciones.TELEFONO)) {
+				mensaje("El numero de teléfono es de 9 digitos");
+		} else if (!dir.matches(Validaciones.TEXTO_NUMERO)) {
+			mensaje("La dirección es de 2 a 20 caracteres");
+		} else if (cboPAIS.getSelectedIndex() == 0) {
+			mensaje("Seleccione el país");
+		} else if (cboCOMPROB.getSelectedIndex() == 0) {
+			mensaje("Seleccione el tipo de Comprobante de Pago");
+		} else {
+			Cliente cli = new Cliente();
+			cli.setDni(dni);
+			cli.setNombre(nom);
+			cli.setApellido(ape);
+			cli.setTelefono(tel);
+			cli.setDireccion(dir);
+			cli.setPais(pai);
+			cli.setComprobante(com);
+			
+
+			ClienteModel model = new ClienteModel();
+			int salida = model.registrarCliente(cli);
+
+			if (salida > 0) {
+				lista();
+				limpiarCajasTexto();
+				mensaje("Cliente Registrado con exito");
+
+			} else {
+				mensaje("Corriga los datos  ingresados");
+			}
+		}
+	}
+	private void busca() {
+		int fila = table .getSelectedRow();
+		idSeleccionado = (Integer)table.getValueAt(fila,0);
+		 String nombre = (String)table.getValueAt(fila, 1);
+		 String apellido = (String)table.getValueAt(fila, 2);
+		 String dni = (String)table.getValueAt(fila, 3);
+		 String telefono = (String)table.getValueAt(fila, 4);
+		 String direccion = (String)table.getValueAt(fila, 5);
+		 String pais = (String)table.getValueAt(fila, 6);
+		 String comprobante = (String)table.getValueAt(fila, 7);
+		 
+		 System.out.println(idSeleccionado + "-" + nombre + "-" + apellido + "-" + dni + "-" + telefono +
+				 			"-" + direccion + "-" + pais + "-" + comprobante);
+		 
+		 txtNOMBRE.setText(nombre);
+		 txtAPELLIDO.setText(apellido);
+		 txtDNI.setText(dni);
+		 txtTELEF.setText(telefono);
+		 txtDIREC.setText(direccion);
+		 cboPAIS.setSelectedItem(pais);
+		 cboCOMPROB.setSelectedItem(comprobante);
+		 }
+	private void elimina() {
+		if(idSeleccionado==-1) {
+			mensaje("Seleccione un fila para eliminar");
+		}else {
+			ClienteModel model = new ClienteModel();
+			int salida = model.eliminacliente(idSeleccionado);
+
+			if (salida > 0) {
+				lista();
+				idSeleccionado = -1;
+				limpiarCajasTexto();
+				mensaje("Se eliminó correctamente");
+			} else {
+				mensaje("Error en la eliminación");
+			}
+		}
+	}
+	private void actualiza() {
+		String nom = txtNOMBRE.getText();
+		String ape = txtAPELLIDO.getText();
+		String dni = txtDNI.getText();
+		String dir = txtDIREC.getText();
+		String tel = txtTELEF.getText();
+		String pai = cboPAIS.getSelectedItem().toString();
+		String com = cboCOMPROB.getSelectedItem().toString();
+		
+		if (idSeleccionado == -1) {
+			mensaje("Seleccione una fila");
+		}else if(!nom.matches(Validaciones.TEXTO)) {
+			mensaje("El nombre es de 2 a 20 caracteres");
+		} else if (!ape.matches(Validaciones.TEXTO)) {
+			mensaje("El apellido es de 2 a 20 caracteres");
+		} else if (!dni.matches(Validaciones.DNI)) {
+			mensaje("El DNI es de 8 digitos");
+		}else if (!tel.matches(Validaciones.TELEFONO)) {
+				mensaje("El numero de teléfono es de 9 digitos");
+		} else if (!dir.matches(Validaciones.TEXTO_NUMERO)) {
+			mensaje("La dirección es de 2 a 20 caracteres");
+		} else if (cboPAIS.getSelectedIndex() == 0) {
+			mensaje("Seleccione el país");
+		} else if (cboCOMPROB.getSelectedIndex() == 0) {
+			mensaje("Seleccione el tipo de Comprobante de Pago");
+		} else {
+			Cliente cli = new Cliente();
+			cli.setIdCliente(idSeleccionado);
+			cli.setDni(dni);
+			cli.setNombre(nom);
+			cli.setApellido(ape);
+			cli.setTelefono(tel);
+			cli.setDireccion(dir);
+			cli.setPais(pai);
+			cli.setComprobante(com);
+			
+
+			ClienteModel model = new ClienteModel();
+			int salida = model.registrarCliente(cli);
+
+			if (salida > 0) {
+				lista();
+				limpiarCajasTexto();
+				mensaje("Cliente Registrado con exito");
+
+			} else {
+				mensaje("Corriga los datos  ingresados");
+			}
+		}
+	}
+	public void keyPressed(KeyEvent e) {
+	}
+	public void keyReleased(KeyEvent e) {
+	}
+	public void keyTyped(KeyEvent e) {
+		if (e.getSource() == txtDIREC) {
+			do_txtDIREC_keyTyped(e);
+		}
+		if (e.getSource() == txtTELEF) {
+			do_txtTELEF_keyTyped(e);
+		}
+		if (e.getSource() == txtDNI) {
+			do_txtDNI_keyTyped(e);
+		}
+		if (e.getSource() == txtAPELLIDO) {
+			do_txtAPELLIDO_keyTyped(e);
+		}
+		if (e.getSource() == txtNOMBRE) {
+			do_txtNOMBRE_keyTyped(e);
+		}
+	}
+	protected void do_txtNOMBRE_keyTyped(KeyEvent e) {
+		if (Character.isDigit(e.getKeyChar())) {
+			e.consume();
+		}
+		String nom = txtNOMBRE.getText() + e.getKeyChar();
+		if (nom.length() > 20) {
+			e.consume();
+		}
+	}
+	protected void do_txtAPELLIDO_keyTyped(KeyEvent e) {
+		if (Character.isDigit(e.getKeyChar())) {
+			e.consume();
+		}
+		String ape = txtAPELLIDO.getText() + e.getKeyChar();
+		if (ape.length() > 20) {
+			e.consume();
+		}
+	}
+	protected void do_txtDNI_keyTyped(KeyEvent e) {
+		if (!Character.isDigit(e.getKeyChar())) {
+			e.consume();
+		}
+		String dni = txtDNI.getText() + e.getKeyChar();
+		if (dni.length() > 8) {
+			e.consume();
+		}
+	}
+	protected void do_txtTELEF_keyTyped(KeyEvent e) {
+		if (!Character.isDigit(e.getKeyChar())) {
+			e.consume();
+		}
+		String dni = txtTELEF.getText() + e.getKeyChar();
+		if (dni.length() > 9) {
+			e.consume();
+		}
+	}
+	protected void do_txtDIREC_keyTyped(KeyEvent e) {
+		String dir = txtDIREC.getText() + e.getKeyChar();
+		if (dir.length() > 20) {
+			e.consume();
+		}
 	}
 }
--- src/main/java/model/ClienteModel.java
+++ src/main/java/model/ClienteModel.java
@@ -2,7 +2,9 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import entidad.Cliente;
@@ -21,8 +23,8 @@ public class ClienteModel {
 			//Se crea la conexcion
 			conn = MySqlDBConexion.getConexion();
 			
-			//Se prepara el sql
-			String sql = "insert into cliente value(?,?,?,?,?,?)";
+			//Se prepara el sql server
+			String sql = "insert into cliente value(?,?,?,?,?,?,?)";
 			pstm = conn.prepareStatement(sql);
 			pstm.setString(1,cli.getDni());
 			pstm.setString(2,cli.getNombre());
@@ -30,6 +32,7 @@ public class ClienteModel {
 			pstm.setString(4,cli.getTelefono());
 			pstm.setString(5,cli.getDireccion());
 			pstm.setString(6,cli.getPais());
+			pstm.setString(7,cli.getComprobante());
 			
 			Log.info(">>>"+pstm);
 			
@@ -48,4 +51,121 @@ public class ClienteModel {
 		}
 		return salida;
 	}
+	public int eliminacliente(int idCliente) {
+
+		Logger log = Logger.getLogger(ClienteModel.class.getName());
+
+		int salida = -1;
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			// PASO 1 : CREAR LA CONEXION
+			conn = MySqlDBConexion.getConexion();
+			// PASO 2 : SE PREPARA EL SQL
+			String sql = "delete from cliente where idCliente = ?";
+			pstm = conn.prepareStatement(sql);
+			pstm.setInt(1, idCliente);
+
+			log.info(">>>" + pstm);
+
+			// PASO 3 : EJECUTAMOS A LA BASE DE DATOS
+			// RETORNA LA CANTIDAD DE RESGISTRADOS EN SALIDA
+			salida = pstm.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null)
+					pstm.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e2) {
+			}
+		}
+
+		return salida;
+	}
+	public int actualizacliente(Cliente obj) {
+
+		Logger log = Logger.getLogger(ClienteModel.class.getName());
+
+		int salida = -1;
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			// PASO 1 : CREAR LA CONEXION
+			conn = MySqlDBConexion.getConexion();
+			// PASO 2 : SE PREPARA EL SQL
+			String sql = "update cliente set nombre=?,apellido=?,dni=?,telefono=?,direccion=?,pais=?,comprobante=? where idCliente=?";
+			pstm = conn.prepareStatement(sql);
+			pstm.setString(1, obj.getNombre());
+			pstm.setString(2, obj.getApellido());
+			pstm.setString(3, obj.getDni());
+			pstm.setString(4, obj.getTelefono());
+			pstm.setString(5, obj.getDireccion());
+			pstm.setString(6, obj.getPais());
+			pstm.setString(7, obj.getComprobante());
+			pstm.setInt(8, obj.getIdCliente());
+
+			log.info(">>>" + pstm);
+
+			// PASO 3 : EJECUTAMOS A LA BASE DE DATOS
+			// RETORNA LA CANTIDAD DE RESGISTRADOS EN SALIDA
+			salida = pstm.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null)
+					pstm.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e2) {
+			}
+		}
+
+		return salida;
+	}
+	public List<Cliente> listaCliente() {
+		ArrayList<Cliente> salida = new ArrayList<Cliente>();
+		
+		Connection conn= null;
+		PreparedStatement pstm= null;
+		ResultSet rs = null;
+		try {
+			// PASO 1 : CREAR LA CONEXION
+			conn = MySqlDBConexion.getConexion();
+			
+			// PASO 2 : SE PREPARA EL SQL
+			String sql = "select * from Cliente";
+			pstm = conn.prepareStatement(sql);
+			
+			// PASO 3 : EJECUTAMOS A LA BASE DE DATOS
+			rs = pstm.executeQuery();
+			Cliente obj = null;
+			while(rs.next()) {
+				obj= new Cliente();
+				obj.setIdCliente(rs.getInt(1));
+				obj.setNombre(rs.getString(2));
+				obj.setApellido(rs.getString(3));
+				obj.setDni(rs.getString(4));
+				obj.setTelefono(rs.getString(5));
+				obj.setDireccion(rs.getString(6));
+				obj.setPais(rs.getString(7));
+				obj.setComprobante(rs.getString(8));
+				salida.add(obj);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (pstm!=null) 
+					pstm.close();
+				if (conn!=null) 
+					conn.close();	
+			} catch (Exception e2) {}
+		}
+		return salida;
+	}
+
 }
